6e9b88bbe42472e6437876f7ac2d6bb45527277f
Oleg Marchenko <trash@gmail.com>
2022-09-05 20:05:31 +0300

    json_out added

--- Class_VKdownloader.py
+++ Class_VKdownloader.py
@@ -5,17 +5,19 @@ class VKdownloader:
     def __init__(self, token):
         self.token = token
         self.url = 'http://api.vk.com/method/'
+        self.version = '5.131'
 
-    def get_photos(self, count=5):
+    def get_photos(self, count=5, album_id='profile'):
         '''
         Загружает фото с профиля, возвращает json c результатами
-        :param count: Количество требуемых результатов
+        :count: Количество требуемых результатов
+        :album_id: Название альбома (по умолчанию фото профиля)
         '''
         url = self.url + 'photos.get'
         params = {
             "access_token": self.token,
-            "album_id": "profile",
-            "v": "5.131",
+            "album_id": album_id,
+            "v": self.version,
             "extended": "1",
             "count": count
         }
@@ -23,12 +25,25 @@ class VKdownloader:
 
         return resp.json()
 
+    def get_albums(self):
+        url = self.url + 'photos.getAlbums'
+        params = {
+            "access_token": self.token,
+            "v": self.version,
+        }
+        resp = requests.get(url, params=params)
+
+        return resp.json()
+
     def __try_it(self):
+        '''
+        Просто тест, не лезьте
+        '''
         url = self.url + 'users.get'
         params = {
             "user_ids": "1",
             "access_token": self.token,
-            "v": "5.131",
+            "v": self.version,
         }
         resp = requests.get(url, params=params)
 
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from Class_VKdownloader import VKdownloader
 from Class_YaUploader import YaUploader
 from datetime import datetime
+from pprint import pprint
 
 if __name__ == "__main__":
 
@@ -9,11 +10,15 @@ if __name__ == "__main__":
 
     photos = VKdownloader(token_vk)
     photos_list = {}
+    json_out = []
     for j in photos.get_photos()["response"]["items"]:
         for i in j['sizes']:
             if i['type'] == 'z':
                 photos_list[i['url']] = [str(j["likes"]["count"]),
                                          datetime.utcfromtimestamp(j["date"]).strftime('%Y-%m-%d')]
+                json_out.append({"file_name": str(j["likes"]["count"])
+                                              + " " +
+                                              datetime.utcfromtimestamp(j["date"]).strftime('%Y-%m-%d'), "size": i['type']})
 
     with open('Tokens/token_ya_disk.txt') as token_file:
         token_ya = token_file.read()
@@ -23,3 +28,5 @@ if __name__ == "__main__":
     upload.create_folder(folder_name)
     for url in photos_list:
         upload.upload_from_url(url, f'{folder_name}/{str(photos_list[url][0] + " " + photos_list[url][1])}')
+
+    pprint(json_out)
